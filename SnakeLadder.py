05b4a867f136f3c11b93a7b89d828d7ab2fcd0c2
Ankursinsinwar <ankursinsinwar2003@gmail.com>
2026-03-10 16:41:36 +0530

    Added: Reporting the number of times the dice was played to win the game and also the position after every die role

--- SnakeLadder.py
+++ SnakeLadder.py
@@ -3,6 +3,7 @@ import random
 pos = 0
 print("Welcome to Snake And Ladder program")
 print("Single player at start position ", pos)
+dice_count = 0
 
 def rollDice():
     dice = random.randint(1,6)
@@ -19,11 +20,15 @@ def option(pos, dice):
         pos-=dice
         if pos<0 :
             pos=0
+    print("pos: ",pos)
     return pos
 
 while(pos<100):
     dice=rollDice()
     pos=option(pos,dice)
+    dice_count += 1
 
 if(pos>=100):
     print("Player win!")
+
+print("Total Dice count: ", dice_count)
