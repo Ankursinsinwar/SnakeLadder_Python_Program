f284a16f494d18046b879bc069bf06d6ccf73b4f
Ankursinsinwar <ankursinsinwar2003@gmail.com>
2026-03-10 15:40:07 +0530

    Added: dice function using random method

--- SnakeLadder.py
+++ SnakeLadder.py
@@ -1,3 +1,9 @@
+import random
+
 pos = 0
 print("Welcome to Snake And Ladder program")
 print("Single player at start position ", pos)
+
+def rollDice():
+    dice = random.randint(1,6)
+    return dice
