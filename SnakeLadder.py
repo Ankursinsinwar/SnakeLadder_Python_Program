004d58f8f708887a9727d3b8970398b7d05c2df6
Ankursinsinwar <ankursinsinwar2003@gmail.com>
2026-03-10 15:54:54 +0530

    Added: SankeLadder Player Checks for option [ no move | ledder | Snake ]

--- SnakeLadder.py
+++ SnakeLadder.py
@@ -7,3 +7,15 @@ print("Single player at start position ", pos)
 def rollDice():
     dice = random.randint(1,6)
     return dice
+
+def option(pos, dice):
+    option=random.randint(0,2)
+    if(option==0):
+        print("No move")
+    elif(option==1):
+        print("Ladder")
+        pos+=dice
+    else:
+        print("Snake")
+        pos-=dice
+    return pos
